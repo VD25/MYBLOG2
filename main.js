4dfd18fb3d64ae28deb5ca782737bb4879370394
VD25 <76209392+VD25@users.noreply.github.com>
2021-04-27 14:46:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function getparagraph1(){
+    var inputs=[];
+    for(var i=1; i<=6; i++){
+inputs.push(document.getElementById("para_input_box_" + i).value);
+inputs.join(".");
+document.getElementById("showparagraph1").innerHTML = inputs.join(".")
+    }}
